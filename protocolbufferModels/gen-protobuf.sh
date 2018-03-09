052a17ffce57ac3a87997ab2f9ce68c4eb28208c
futang yang <yangfutang@91guoxin.com>
2018-03-09 15:47:40 +0800

    提交td-长连接模型

--- protocolbufferModels/gen-protobuf.sh
+++ protocolbufferModels/gen-protobuf.sh
@@ -8,13 +8,20 @@ protoc --plugin=/usr/local/bin/protoc-gen-objc $file --objc_out=${SRCROOT}/proto
 done
 
 
-
-
 for file in ${SRCROOT}/protocolbufferModels/protobufModels/jujin-Trade/*
 do
 echo $file is file path \! ;
 protoc --plugin=/usr/local/bin/protoc-gen-objc $file --objc_out=${SRCROOT}/protocolbufferModels/jujjinProtocolbufferModels/jujin-Trade/ --proto_path=${SRCROOT}/protocolbufferModels/protobufModels/jujin-Trade/
 done
+
+
+
+for file in ${SRCROOT}/protocolbufferModels/protobufModels/td-Hq/*
+do
+echo $file is file path \! ;
+protoc --plugin=/usr/local/bin/protoc-gen-objc $file --objc_out=${SRCROOT}/protocolbufferModels/td-ProtocolbufferModels/td-Hq/ --proto_path=${SRCROOT}/protocolbufferModels/protobufModels/td-Hq/
+done
+
 
 
 
